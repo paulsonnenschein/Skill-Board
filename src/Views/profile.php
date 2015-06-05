6f24d4c2875bc66c92450ddbeaa5e3e4c17a6e68
unknown <Andreas.schneebeli@ICTW-RE-WO-214.ict-werk.ch>
2015-06-05 13:57:09 +0200

    Updated CSS

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,1 +1,32 @@
-<h1>Index</h1>
+  	  	
+			<div class="jumbotron" id="userBgJumbo">
+				<div class="circletag"> </div>
+
+			</div>
+			<div class="container">
+	<div class="row">
+		<div class="col-lg-12">
+			<div class="col-lg-4">
+				<div class="jumbotron">
+					<h3>Über NAME</h3>
+					<p>Name:</p>
+					<p>Alter:</p>
+					<p>Hobbies:</p>
+					<p>Name:</p>
+				
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="jumbotron">
+				
+				</div>
+			</div>
+			<div class="col-lg-4">
+				<div class="jumbotron">
+				
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
