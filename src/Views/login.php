c6ef29bf75acc44bf91f23c0d01845885d534152
Noah Denger <noah.denger@gibmit.ch>
2015-06-11 08:19:34 +0200

    Changed placeholder 'Benutzername' -> 'E-Mail'

--- src/Views/login.php
+++ src/Views/login.php
@@ -1,11 +1,10 @@
 <div class="jumbotron" id="loginJumbo">
     <div class="container">
         <div class="login-container">
-            <div id="output"></div>
             <div class="brand"></div>
             <div class="form-box">
                 <form action="" method="">
-                    <input name="user" placeholder="Benutzername">
+                    <input name="user" placeholder="E-Mail">
                     <input name="password" type="password" placeholder="Passwort">
                     <button class="btn btn-info btn-block login" type="submit">Login</button>
                 </form>
