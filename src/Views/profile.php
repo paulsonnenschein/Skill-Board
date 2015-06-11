881f7d0196131df4eeed1ee7e519848b2db464c0
Noah Denger <noah.denger@gibmit.ch>
2015-06-11 08:19:56 +0200

    Removed jumbotrons

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,32 +1,23 @@
-			<div class="circletag"> </div>
-			<div class="jumbotron" id="userBgJumbo">
+            <div class="circletag"> </div>
+            <div class="jumbotron" id="userBgJumbo">
 
-			</div>
-	
-			<div class="container">
-	<!--<div class="row">
-		<div class="col-lg-12">
-			<div class="col-lg-4">
-				<div class="jumbotron">
-					<h3>Über NAME</h3>
-					<p>Name:</p>
-					<p>Alter:</p>
-					<p>Hobbies:</p>
-					<p>Name:</p>
-				
-				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="jumbotron">
-				
-				</div>
-			</div>
-			<div class="col-lg-4">
-				<div class="jumbotron">
-				
-				</div>
-			</div>
-			
-		</div>
-	</div>
-</div>-->
+            </div>
+
+            <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="col-md-4">
+                    <h3>Über NAME</h3>
+                    <p>Name:</p>
+                    <p>Alter:</p>
+                    <p>Hobbies:</p>
+                    <p>Name:</p>
+                            </div>
+            <div class="col-md-4">
+            </div>
+            <div class="col-md-4">
+            </div>
+
+        </div>
+    </div>
+</div>
