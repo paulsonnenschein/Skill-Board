9ac5f942a571ac20f316b333738db42761383413
andreasschneebeli <Andreas.Schneebeli@gibmit.ch>
2015-06-11 11:25:26 +0200

    updated profile view

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,7 +1,6 @@
-            <div class="circletag"> </div>
-            <div class="jumbotron" id="userBgJumbo">
+			<div class="circletag"> </div>
+			<div class="jumbotron" id="userBgJumbo">
 
-<<<<<<< HEAD
 			</div>
 	
 			<div class="container">
@@ -72,25 +71,3 @@
 
 		</div>
 </div>
-=======
-            </div>
-
-            <div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            <div class="col-md-4">
-                    <h3>Über NAME</h3>
-                    <p>Name:</p>
-                    <p>Alter:</p>
-                    <p>Hobbies:</p>
-                    <p>Name:</p>
-                            </div>
-            <div class="col-md-4">
-            </div>
-            <div class="col-md-4">
-            </div>
-
-        </div>
-    </div>
-</div>
->>>>>>> origin/master
