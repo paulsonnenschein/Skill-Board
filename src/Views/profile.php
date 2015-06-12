a2ac6dd9dc58328d571dbb3d0070a123603f8215
Patrick Reimers <patrick@reimers.me>
2015-06-12 13:10:42 +0200

    Added Show Profile

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,28 +1,23 @@
 <div class="jumbotron" id="jumboProfile">
 	<div id="hovercard">
 		<div id="avatar">
-			<img src="assets/images/bg2.jpg" />
+			<img alt="" src="https://secure.gravatar.com/avatar/<?php echo $this->user['gravatar']; ?>?d=wavatar&s=150">
 		</div>
 	</div>
-		<div id="info">
-			<div id="title">
-				<a target="_blank" href="http://scripteden.com/">Script Eden</a>
-			</div>
-			<div id="desc">Passionate designer</div>
-			<div id="desc">Curious developer</div>
-			<div id="desc">Tech geek</div>
-		</div>
-</div><br><br>				
+    <div id="info">
+        <div id="title">
+            <a target="_blank" href="#"><?php echo $this->user['name']; ?></a>
+        </div>
+    </div>
+</div>
+<br>
 <div class="container">
 	<div class ="col-lg-12">
 		<div class="col-md-4 col-sm-6">
 			<div class="panel panel-default">
-			   <div class="panel-heading"><h4>Profil</h4></div>
+			   <div class="panel-heading"><h4>Beschreibung</h4></div>
 				<div class="panel-body">
-					<p><span class="glyphicon glyphicon-user"></span> Max Mustermann</p>
-					<p><span class="glyphicon glyphicon-envelope"></span> Max.Mustermann@gmail.com</p>
-					<h3>Beschreibung</h3>
-					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+					<p><?php echo nl2br($this->user['description']); ?></p>
 				</div>
 			 </div> 
 		</div>
@@ -31,21 +26,62 @@
 				<div class="panel panel-default">
 					<div class="panel-heading" role="tab" id="headingOne">
 					  <h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+						<a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
 						  <h4>Projekte <span class="glyphicon glyphicon-menu-down"></span></h4>
 						</a>
 					  </h4>
 					</div>
-					<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-					  <div class="panel-body">
-						<dl class="dl-horizontal">
-						  <dt><a href="#">ProjektName</a></dt>
-						  <dd>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</dd>
-						  <dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>
-						</dl>
-					  </div>
-					</div>
+                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                            <p>
+                                <?php
+                                $projects = '';
+                                if(count($this->user['project']) > 0){
+                                    foreach($this->user['project'] AS $project) {
+                                        $projects .= '<dl class="dl-horizontal">';
+                                        $projects .=     '<dt><a href="project/view/'.$project['id'].'">' . $project['name'] . '</a></dt>';
+                                        $projects .=     '<!--<dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->';
+                                        $projects .= '</dl>';
+                                    }
+                                } else {
+                                    $projects = 'Keine Projekte';
+                                }
+                                echo $projects;
+                                ?>
+                            </p>
+                        </div>
+                    </div>
 				</div>
+
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingThree">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion1" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                <h4>Matches <span class="glyphicon glyphicon-menu-down"></span></h4>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseThree" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingThree">
+                        <div class="panel-body">
+                            <p>
+                                <?php
+                                $matches = '';
+                                if(count($this->user['match']) > 0) {
+                                    foreach ($this->user['match'] AS $match) {
+                                        $matches .= '<dl class="dl-horizontal">';
+                                        $matches .= '<dt><a href="project/view/'.$match['id'].'">' . $match['name'] . '</a></dt>';
+                                        $matches .= '<!--<dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->';
+                                        $matches .= '</dl>';
+                                    }
+                                } else {
+                                    $matches = 'Keine Matches';
+                                }
+                                echo $matches;
+                                ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
 			 </div>
 		</div>
 		<div class="col-md-4 col-sm-6">
@@ -60,7 +96,15 @@
 					</div>
 					<div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
 					  <div class="panel-body">
-						<p><kbd>HTML 5</kbd>, <kbd>PHP 5</kbd>, <kbd>CSS 3</kbd></p>
+						<p>
+							<?php
+							$skills = '';
+							foreach($this->user['skill'] AS $skill){
+								$skills .= '<kbd>'.$skill['name'].'</kbd> ';
+							}
+							echo substr($skills,0,-1);
+							?>
+						</p>
 					  </div>
 					</div>
 				</div>
--- src/lib/User.php
+++ src/lib/User.php
@@ -92,14 +92,40 @@ class User {
 
     public function getProfile($id){
         $user = $this->getUserInfo($id);
+
+        // Skills
         $sql = "SELECT `programminglanguages`.`name` AS `name` FROM `skills` LEFT JOIN `programminglanguages` ON (`programminglanguages`.`id` = `skills`.`ProgrammingLanguages_id`) WHERE `skills`.`User_id`='".$user['id']."' ORDER BY `name` ASC";
         $statement = $this->db->query($sql);
-        $skill = $statement->fetchAll();
+        $skills = $statement->fetchAll();
 
+        // Matches
+        $sql = "SELECT DISTINCT `project`.`id` AS `id`, `project`.`name` AS `name` FROM `project`
+                JOIN `requirements` ON (`requirements`.`Project_id` = `project`.`id`)
+                LEFT JOIN `developer` ON (`developer`.`Project_id` = `Project`.`id`)
+                WHERE `requirements`.`ProgrammingLanguages_id` IN (
+                    SELECT `skills`.`ProgrammingLanguages_id` FROM `user`
+                    JOIN `skills` ON (`skills`.`User_id` = `user`.`id`)
+                    WHERE `user`.`id` = ".$user['id'].")
+                AND (`developer`.`User_id` != ".$user['id']." OR `developer`.`User_id` IS NULL)";
+        $statement = $this->db->query($sql);
+        $matches = $statement->fetchAll();
+
+        // Projects
+        $sql = "SELECT `project`.`id` AS `id`, `project`.`name` AS `name` FROM `developer`
+                LEFT JOIN `user` ON (`user`.`id` = `developer`.`User_id`)
+                LEFT JOIN `project` ON (`project`.`id` = `developer`.`Project_id`)
+                WHERE `user`.`id` = ".$user['id'];
+        $statement = $this->db->query($sql);
+        $projects = $statement->fetchAll();
+
+        // Output
         $array = array(
+            'gravatar' => md5(strtolower(trim($user['email']))),
             'name' => $user['firstName'].' '.$user['lastName'],
             'description' => $user['description'],
-            'skill' => $skill
+            'match' => $matches,
+            'project' => $projects,
+            'skill' => $skills
         );
 
         return $array;
