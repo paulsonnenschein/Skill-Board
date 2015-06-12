e40b001f2ba692ad195c5df10950560ac8d19f4a
Paul Sonnenschein <paulsonnenschein0@gmail.com>
2015-06-12 18:00:04 +0200

    fixed profile page

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,63 +1,65 @@
 <div class="jumbotron" id="jumboProfile">
-	<div id="hovercard">
-		<div id="avatar">
+    <div id="hovercard">
+        <div id="avatar">
             <img alt="" src="https://secure.gravatar.com/avatar/<?php echo $this->user['gravatar']; ?>?d=wavatar&s=150">
-		</div>
-	</div>
-		<div id="info">
-			<div id="title">
-				<h2><?php echo $this->escape($this->user['name']); ?><br /><a href="profile/edit"><span class="glyphicon glyphicon-cog"></span></a> <a href="project/new"><span class="glyphicon glyphicon-plus"></span></a></h2>
-			</div>
-		</div>
-</div><br><br>				
+        </div>
+    </div>
+    <div id="info">
+        <div id="title">
+            <h2>
+                <?php echo $this->escape($this->user['name']); ?>
+                <br/>
+                <a href="profile/edit">
+                    <span class="glyphicon glyphicon-cog"></span>
+                </a>
+                <a href="project/new">
+                    <span class="glyphicon glyphicon-plus"></span>
+                </a>
+            </h2>
+        </div>
+    </div>
+</div>
+<br/>
+<br/>
 <div class="container">
-	<div class ="col-lg-12">
-		<div class="col-md-4 col-sm-6">
-			<div class="panel panel-default">
-			   <div class="panel-heading"><h4>Beschreibung</h4></div>
-				<div class="panel-body">
-					<p><?php echo nl2br($this->user['description']); ?></p>
-				</div>
-			 </div> 
-		</div>
-		<div class="col-md-4 col-sm-6">
-			<div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
-				<div class="panel panel-default">
-					<div class="panel-heading" role="tab" id="headingOne">
-					  <h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-						  <h4>Projekte <span class="glyphicon glyphicon-menu-down"></span></h4>
-						</a>
-					  </h4>
-					</div>
-					<div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
-					  <div class="panel-body">
-						<dl class="dl-horizontal">
-                            <p>
-                                <?php
-                                $projects = '';
-                                if(count($this->user['project']) > 0){
-                                    foreach($this->user['project'] AS $project) {
-                                        $projects .= '<dl class="dl-horizontal">';
-                                        $projects .=     '<dt><a href="project/view/'.$project['id'].'">' . $project['name'] . '</a></dt>';
-                                        $projects .=     '<!--<dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->';
-                                        $projects .= '</dl>';
-                                    }
-                                } else {
-                                    $projects = 'Keine Projekte';
-                                }
-                                echo $projects;
-                                ?>
-                            </p>
-
-						  <!--<dt><a href="#">ProjektName</a></dt>
-						  <dd>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</dd>
-						  <dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->
-						</dl>
-					  </div>
-					</div>
-				</div>
-			 </div>
+    <div class="col-lg-12">
+        <div class="col-md-4 col-sm-6">
+            <div class="panel panel-default">
+                <div class="panel-heading"><h4>Beschreibung</h4></div>
+                <div class="panel-body">
+                    <p><?= nl2br($this->escape($this->user['description'])); ?></p>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingOne">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true"aria-controls="collapseOne">
+                                <h4>Projekte <span class="glyphicon glyphicon-menu-down"></span></h4>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseOne" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingOne">
+                        <div class="panel-body">
+                            <ul class="list-unstyled">
+                                <?php if (count($this->user['project']) > 0): ?>
+                                    <?php foreach ($this->user['project'] as $project): ?>
+                                        <li>
+                                            <a href="project/view/'<?= $this->escape($project['id']) ?>">
+                                                <?= $this->escape($project['name']) ?>
+                                            </a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                <?php else: ?>
+                                    <p>Keine Projekte!</p>
+                                <?php endif; ?>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
 
             <div class="panel-group" id="accordion3" role="tablist" aria-multiselectable="true">
                 <div class="panel panel-default">
@@ -70,58 +72,45 @@
                     </div>
                     <div id="collapseThree" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingThree">
                         <div class="panel-body">
-                            <dl class="dl-horizontal">
-                                <p>
-                                    <?php
-                                    $matches = '';
-                                    if(count($this->user['match']) > 0){
-                                        foreach($this->user['match'] AS $match) {
-                                            $matches .= '<dl class="dl-horizontal">';
-                                            $matches .=     '<dt><a href="project/view/'.$match['id'].'">' . $match['name'] . '</a></dt>';
-                                            $matches .=     '<!--<dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->';
-                                            $matches .= '</dl>';
-                                        }
-                                    } else {
-                                        $matches = 'Keine Projekte';
-                                    }
-                                    echo $matches;
-                                    ?>
-                                </p>
-
-                                <!--<dt><a href="#">ProjektName</a></dt>
-                                <dd>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor</dd>
-                                <dd><span class="glyphicon glyphicon-calendar"><b>12.06.2015</b></span></dd>-->
-                            </dl>
+                            <ul class="list-unstyled">
+                                <?php if (count($this->user['match']) > 0): ?>
+                                    <?php foreach ($this->user['match'] as $match): ?>
+                                        <li>
+                                            <a href="project/view/'<?= $this->escape($match['id']) ?>">
+                                                <?= $this->escape($match['name']) ?>
+                                            </a>
+                                        </li>
+                                    <?php endforeach; ?>
+                                <?php else: ?>
+                                    <p>Keine passenden Projekte!</p>
+                                <?php endif; ?>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
-		</div>
-		<div class="col-md-4 col-sm-6">
-			<div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
-				<div class="panel panel-default">
-					<div class="panel-heading" role="tab" id="headingTwo">
-					  <h4 class="panel-title">
-						<a data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-						  <h4>Skills <span class="glyphicon glyphicon-menu-down"></span></h4>
-						</a>
-					  </h4>
-					</div>
-					<div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
-					  <div class="panel-body">
-                          <p>
-                              <?php
-                              $skills = '';
-                              foreach($this->user['skill'] AS $skill){
-                                  $skills .= '<kbd>'.$skill['name'].'</kbd> ';
-                              }
-                              echo substr($skills,0,-1);
-                              ?>
-                          </p>
-					  </div>
-					</div>
-				</div>
-			 </div>
-		</div>
-	</div>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
+                <div class="panel panel-default">
+                    <div class="panel-heading" role="tab" id="headingTwo">
+                        <h4 class="panel-title">
+                            <a data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+                                <h4>Skills <span class="glyphicon glyphicon-menu-down"></span></h4>
+                            </a>
+                        </h4>
+                    </div>
+                    <div id="collapseTwo" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="headingTwo">
+                        <div class="panel-body">
+                            <p>
+                                <?php foreach ($this->user['skill'] as $skill): ?>
+                                    <kbd><?= $this->escape($skill['name']) ?></kbd>
+                                <?php endforeach; ?>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
