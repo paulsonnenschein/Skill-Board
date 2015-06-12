d1f296ed669747ad70c8eadf7c24a19b1aafde2f
andreasschneebeli <Andreas.Schneebeli@gibmit.ch>
2015-06-12 10:29:05 +0200

    Updated CSS & Profile

--- src/Views/profile.php
+++ src/Views/profile.php
@@ -1,65 +1,66 @@
 <div class="jumbotron" id="jumboProfile">
-	<div id="info">
-		<div id="title">
-			<a target="_blank" href="http://scripteden.com/">Script Eden</a>
+	<div id="hovercard">
+		<div id="avatar">
+			<img src="assets/images/bg2.jpg" />
 		</div>
-		<div id="desc">Passionate designer</div>
-		<div id="desc">Curious developer</div>
-		<div id="desc">Tech geek</div>
+	</div>
+		<div id="info">
+			<div id="title">
+				<a target="_blank" href="http://scripteden.com/">Script Eden</a>
+			</div>
+			<div id="desc">Passionate designer</div>
+			<div id="desc">Curious developer</div>
+			<div id="desc">Tech geek</div>
+		</div>
+</div><br><br>				
+<div class="container">
+	<div class ="col-lg-12">
+		<div class="col-md-4 col-sm-6">
+			<div class="panel panel-default">
+			   <div class="panel-heading"><h4>Profil</h4></div>
+				<div class="panel-body">
+					<p><span class="glyphicon glyphicon-user"></span> Max Mustermann</p>
+					<p><span class="glyphicon glyphicon-envelope"></span> Max.Mustermann@gmail.com</p>
+					<h3>Beschreibung</h3>
+					<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+				</div>
+			 </div> 
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingOne">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+						  <h4>Projekte <span class="glyphicon glyphicon-menu-down"></span></h4>
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
+					  <div class="panel-body">
+						Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					  </div>
+					</div>
+				</div>
+			 </div>
+		</div>
+		<div class="col-md-4 col-sm-6">
+			<div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
+				<div class="panel panel-default">
+					<div class="panel-heading" role="tab" id="headingTwo">
+					  <h4 class="panel-title">
+						<a data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+						  <h4>Skills <span class="glyphicon glyphicon-menu-down"></span></h4>
+						</a>
+					  </h4>
+					</div>
+					<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
+					  <div class="panel-body">
+						<p><kbd>HTML 5</kbd>, <kbd>PHP 5</kbd>, <kbd>CSS 3</kbd></p>
+					  </div>
+					</div>
+				</div>
+			 </div>
+		</div>
 	</div>
 </div>
-
-
-					
-					<br>
-					<div class="container">
-						<div class ="col-lg-12">
-							<div class="col-md-4 col-sm-6">
-								<div class="panel panel-default">
-								   <div class="panel-heading"><h4>Profil</h4></div>
-									<div class="panel-body">
-										<p><span class="glyphicon glyphicon-user"></span> Max Mustermann</p>
-										<p><span class="glyphicon glyphicon-envelope"></span> Max.Mustermann@gmail.com</p>
-										<h3>Beschreibung</h3>
-										<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-									</div>
-								 </div> 
-							</div>
-							<div class="col-md-4 col-sm-6">
-								<div class="panel-group" id="accordion1" role="tablist" aria-multiselectable="true">
-								    <div class="panel panel-default">
-										<div class="panel-heading" role="tab" id="headingOne">
-										  <h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordion1" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
-											  <h4>Projekte <span class="glyphicon glyphicon-menu-down"></span></h4>
-											</a>
-										  </h4>
-										</div>
-										<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne">
-										  <div class="panel-body">
-											Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-										  </div>
-										</div>
-								    </div>
-								 </div>
-							</div>
-							<div class="col-md-4 col-sm-6">
-								<div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
-								    <div class="panel panel-default">
-										<div class="panel-heading" role="tab" id="headingTwo">
-										  <h4 class="panel-title">
-											<a data-toggle="collapse" data-parent="#accordion2" href="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
-											  <h4>Skills <span class="glyphicon glyphicon-menu-down"></span></h4>
-											</a>
-										  </h4>
-										</div>
-										<div id="collapseTwo" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingTwo">
-										  <div class="panel-body">
-											<p><kbd>HTML 5</kbd>, <kbd>PHP 5</kbd>, <kbd>CSS 3</kbd></p>
-										  </div>
-										</div>
-								    </div>
-								 </div>
-							</div>
-						</div>
-					</div>
