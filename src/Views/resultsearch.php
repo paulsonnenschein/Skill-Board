2b65e883bcb371d178eb2311c4dbfeecf1f05103
Noah Denger <noah.denger@gibmit.ch>
2015-06-12 11:11:03 +0200

    Added container to resultsearch

--- src/Views/resultsearch.php
+++ src/Views/resultsearch.php
@@ -1,10 +1,12 @@
-<h1>User List </h1><br>
-<?php foreach($this->userList as $user): ?>
-	<a href="user/<?= $user['id']?>"><?= $user['firstname'].' '.$user['lastname']?></a><br>
+<div class="container">
+    <h1>User List </h1><br>
+    <?php foreach($this->userList as $user): ?>
+        <a href="user/<?= $user['id']?>"><?= $user['firstname'].' '.$user['lastname']?></a><br>
 
-<?php endforeach; ?>
-<h1>Project List </h1><br>
-<?php foreach($this->projectList as $project): ?>
-	<a href="project/<?= $project['id']?>"><?= $project['search']?></a><br>
+    <?php endforeach; ?>
+    <h1>Project List </h1><br>
+    <?php foreach($this->projectList as $project): ?>
+        <a href="project/<?= $project['id']?>"><?= $project['search']?></a><br>
 
-<?php endforeach; ?>
+    <?php endforeach; ?>
+</div>
--- src/Views/search.php
+++ src/Views/search.php
@@ -1,13 +1,13 @@
 <div id="searchJumbo">
-	<div class="container">
-		<div class="search-container">
-			<div class="search-form-box">
-				<form method="POST">
-					<input class="top" type="text" name="query" placeholder="Suche">
-					<input class="bottom" type="submit" value ="Go!">
-				</form>
-			</div>
-		</div>
-	
-	</div>
+    <div class="container">
+        <div class="search-container">
+            <div class="search-form-box">
+                <form method="POST">
+                    <input class="top" type="text" name="query" placeholder="Suche">
+                    <input class="bottom" type="submit" value ="Go!">
+                </form>
+            </div>
+        </div>
+
+    </div>
 </div>
