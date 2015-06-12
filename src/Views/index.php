21e993b0caa80e8d03d40df10794c8b0b18056f3
Noah Denger <noah.denger@gibmit.ch>
2015-06-12 11:09:31 +0200

    fixed index view

--- src/Views/index.php
+++ src/Views/index.php
@@ -1,42 +1,6 @@
-<div class="carousel fade-carousel slide" data-ride="carousel" data-interval="4000" id="bs-carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-        <li data-target="#bs-carousel" data-slide-to="0" class="active"></li>
-        <li data-target="#bs-carousel" data-slide-to="1"></li>
-        <li data-target="#bs-carousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <div class="item slides active">
-            <div class="slide-1"></div>
-            <div class="hero">
-                <hgroup>
-                    <h1>Individuelle Suchergebnisse</h1>
-                    <h3>Wir suchen Projekte, welche speziell auf Sie abgestimmt sind</h3>
-                </hgroup>
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-            </div>
-        </div>
-        <div class="item slides">
-            <div class="slide-2"></div>
-            <div class="hero">
-                <hgroup>
-                    <h1>Flexibilität</h1>
-                    <h3>Get start your next awesome project</h3>
-                </hgroup>
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-            </div>
-        </div>
-        <div class="item slides">
-            <div class="slide-3"></div>
-            <div class="hero">
-                <hgroup>
-                    <h1>Blabla</h1>
-                    <h3>Get start your next awesome project</h3>
-                </hgroup>
-                <button class="btn btn-hero btn-lg" role="button">See all features</button>
-            </div>
-        </div>
+<div id="indexBG">
+    <div class="container" id="containerFixIndex">
+        <h1>Test</h1>
+        <h3>Projektbeschrieb</h3>
     </div>
 </div>
