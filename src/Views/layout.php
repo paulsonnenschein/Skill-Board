ff8aae3937d0ea6763cf9671a83ed5246330dd16
Noah Denger <noah.denger@gibmit.ch>
2015-06-12 09:29:15 +0200

    Here you can find the menu in it's final form

--- src/Views/layout.php
+++ src/Views/layout.php
@@ -34,16 +34,37 @@
     <script type="text/javascript" src="assets/javascript/bluring.js"></script>
 </head>
 <body>
-    <div class="menu">
-        <ul class="list-inline">
-            <span>
-                <img src="assets/images/logo.png" width="100px">
-            </span>
-            <li><a href="">Home</a></li>
-            <li><a href="profile">Mein Profil</a></li>
-            <li><a href="search">Suche</a></li>
-        </ul>
-    </div>
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href=""><span class="glyphicon glyphicon-"></span>Skill-Board</a>
+            </div>
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul class="nav navbar-nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="search">Suche</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><span class="glyphicon glyphicon-user"></span> <span class="caret"></span></a>
+                        <ul class="dropdown-menu" role="menu">
+                            <li><a href="profile">Profil</a></li>
+                            <li class="divider"></li>
+                            <li><a href="logout">Abmelden</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-fluid -->
+    </nav>
 
     <div class="container">
         <div><!-- Flashes -->
