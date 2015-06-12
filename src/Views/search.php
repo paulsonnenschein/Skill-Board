d87a896bc636d015f27c21a86292464ed59107c8
Noah Denger <noah.denger@gibmit.ch>
2015-06-12 11:12:10 +0200

    Changed Search Button Value

--- src/Views/search.php
+++ src/Views/search.php
@@ -4,7 +4,7 @@
             <div class="search-form-box">
                 <form method="POST">
                     <input class="top" type="text" name="query" placeholder="Suche">
-                    <input class="bottom" type="submit" value ="Go!">
+                    <input class="bottom" type="submit" value ="Suche Starten">
                 </form>
             </div>
         </div>
